5cd2a42bda1beea77e923edd59b4d33be896ebe7
gibs0nnn <gibs0n@knights.ucf.edu>
2021-08-31 03:17:26 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Nathaniel Gibson
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String input = scanner.next();
+        System.out.println(input + " has " + input.length() + " characters.");
+    }
+}
